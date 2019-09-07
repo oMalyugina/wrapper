b486430cd667e244f632e43f1d7451c612b3a7eb
oMalyugina <o.v.malyugina@gmail.com>
2019-09-07 12:57:56 +0200

    make preproccesing. with errors

--- concrete_wrapper.py
+++ concrete_wrapper.py
@@ -4,15 +4,28 @@ import numpy as np
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
-
+from sklearn.preprocessing import LabelEncoder, Normalizer
 
 class LogRegWrapper(Wrapper):
     def __init__(self):
         super().__init__()
         self.model = LogisticRegression(class_weight='balanced', random_state=123)
 
+    def _label_encoding(self, X):
+        label_encoder = LabelEncoder()
+        for var_name in X.select_dtypes(include=['object']):
+            X[var_name] = label_encoder.fit_transform(X[var_name].astype(str))
+        return X
+
+    def _preprossecing_X(self, X):
+        X = self._label_encoding(X)
+        X = self._handling_missed_value(X)
+        X = self._normalized(X)
+        X = self._feature_selection(X)
+
     def fit(self, X, y):
-        self.model.fit(X, y)
+        X = self._preprossecing_X(X)
+        self.model.fit(X.values, y)
 
     def predict(self, X):
         self.predict(X)
@@ -31,4 +44,27 @@ class LogRegWrapper(Wrapper):
         clf = GridSearchCV(self.model, tuned_parameters, cv=5)
         clf.fit(X, y)
         sorted(clf.cv_results_.keys())
+
+    def _handling_missed_value(self, X):
+        all_data_na = (X.isnull().sum() / len(X)) * 100
+        for name, ratio in all_data_na.items():
+            if ratio > 10:
+                X.drop(name, axis=1)
+        return X.fillna(-1)
+
+    def _normalized(self, X):
+        scaler = Normalizer()
+        return scaler.fit_transform(X)
+
+    def _feature_selection(self, X):
+        return X
+
+if __name__ == '__main__':
+    path_to_data = '../DR_Demo_Lending_Club_reduced.csv'
+    data = pd.read_csv(path_to_data)
+    y = data.is_bad.values
+    X = data.drop('is_bad', axis=1)
+    X.head()
+    lg = LogRegWrapper()
+    lg.fit(X, y)
 
