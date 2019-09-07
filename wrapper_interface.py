140d35f1425440710504702fd5db0b159e639ba8
oMalyugina <o.v.malyugina@gmail.com>
2019-09-07 09:30:01 +0200

    make interface

---
+++ wrapper_interface.py
@@ -0,0 +1,29 @@
+from abc import ABCMeta, abstractmethod
+
+
+class Wrapper:
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def fit(self, X, y):
+        pass
+
+    @abstractmethod
+    def predict(self, X):
+        pass
+
+    @abstractmethod
+    def predict_proba(self, X):
+        pass
+
+    @abstractmethod
+    def evaluate(self, X, y):
+        pass
+
+    @abstractmethod
+    def tune_parameters(self, X, y):
+        pass
